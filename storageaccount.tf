efdebf2f9f9044a54b5a54d23ed663891ad29827
akhilschandran4 <akhilschandran4@gmail.com>
2021-06-21 14:29:03 +0530

    resource

--- storageaccount.tf
+++ storageaccount.tf
@@ -4,11 +4,16 @@ terraform {
     container_name = "terraform-state"
     key = "terraform.tfstate"
     resource_group_name  = "myresourcegroup111111"
-    subscription_id = "00000000-0000-0000-0000-000000000000"
+   /*
+    subscription_id = "3457d5d0-1535-40c6-85fb-cb1919104698"
     client_id = "00000000-0000-0000-0000-000000000000"
     client_secret = "mysecret"
-    tenant_id = "00000000-0000-0000-0000-000000000000"
+    tenant_id = "00000000-0000-0000-0000-000000000000"*/
   }
+}
+resource "azurerm_resource_group" "example" {
+  name     = "myresourcegroup111111"
+  location = "West Europe"
 }
 data "azurerm_storage_account" "mystorageaccount11111" {
   name                = "mystorageaccount11111"
